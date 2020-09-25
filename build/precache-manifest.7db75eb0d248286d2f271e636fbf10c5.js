71e777c81644fb5bb068c009d48e03800a1d7b0c
Merey <mtemirkas@gmail.com>
2020-09-25 09:11:47 +0600

    create: add new function

---
+++ build/precache-manifest.7db75eb0d248286d2f271e636fbf10c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1029e5a7577de07f84e1cbf1dd496a49",
+    "url": "/To-do-list/index.html"
+  },
+  {
+    "revision": "99524cf7c6fe917361fc",
+    "url": "/To-do-list/static/css/main.f78e6d27.chunk.css"
+  },
+  {
+    "revision": "a7c31d6321deb2f9c96a",
+    "url": "/To-do-list/static/js/2.3825438a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-do-list/static/js/2.3825438a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99524cf7c6fe917361fc",
+    "url": "/To-do-list/static/js/main.61547290.chunk.js"
+  },
+  {
+    "revision": "de80078b810d3d3add29",
+    "url": "/To-do-list/static/js/runtime-main.e3cf020e.js"
+  }
+]);
